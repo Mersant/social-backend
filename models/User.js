2d182acb85c43fbbff945fa36d5fa455aa843e68
Mersant <dlowry99@outlook.com>
2022-09-27 16:55:39 -0400

    Update User.js
    Added user model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,28 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-    
-})
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate: {
+            validator: function(a) {
+                return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(a);
+            }, 
+        },
+    },
+    toJSON: {
+        getters: true,
+    },
+    id: false, 
+});
+
+module.exports = userSchema;
