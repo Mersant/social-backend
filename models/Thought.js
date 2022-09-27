465fe0de201b1beb5a83f784ec3c30640d5e9692
Mersant <dlowry99@outlook.com>
2022-09-27 17:26:48 -0400

    Update Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,34 @@
 const { Schema, model } = require('mongoose');
 
 const thoughtSchema = new Schema({
-    
-})
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [
+        {
+                reaction, // Reaction schema reference
+        },
+    ],
+    toJSON: {
+        virtuals: true,
+    },
+});
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
